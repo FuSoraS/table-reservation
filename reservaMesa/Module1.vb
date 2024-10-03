3adc595d2e7c32f812bb95fb9db3b19af1dd1e3d
FuSoraS <developer.FuSoraS@gmail.com>
2024-10-03 16:57:08 -0300

    feat: variables inicializadas con un menu y su ingreso de datos

--- reservaMesa/Module1.vb
+++ reservaMesa/Module1.vb
@@ -1,6 +1,47 @@
 ﻿Module Module1
 
     Sub Main()
+        ' tamaño de cada mesa
+        Dim tableSize4 As Integer = 4
+        Dim tableSize8 As Integer = 8
+        Dim tableSize12 As Integer = 12
+        ' Reservas
+        Dim reserva1() As String
+        ReDim Preserve reserva1(4)
+
+        ' Falta un bucle para que el programa no termine
+        Console.WriteLine("1. Agregar Reservas.
+2. Mostrar Reservas.
+3. Buscar Reserrvas
+4. Eliminar o cancelar reservas
+5. Mostrar distribucion de mesas
+6. Reportes")
+        Console.WriteLine("Ingrese una opcion: ")
+        Dim opcion As Integer = Console.ReadLine()
+        Select Case opcion
+            Case 1
+                Console.Write("Ingrese su nombre: ")
+                reserva1(0) = Console.ReadLine()
+                Console.Write("Ingrese su telefono: ")
+                reserva1(1) = Console.ReadLine()
+                Console.Write("Ingrese la cantidad de personas: ")
+                reserva1(2) = Console.ReadLine()
+                Console.Write("Ingrese numero de comensales: ")
+                reserva1(3) = Console.ReadLine()
+                Console.Write("Ingrese el rut: ")
+                reserva1(4) = Console.ReadLine()
+            Case 2
+                Console.WriteLine("Nombre: " & reserva1(0))
+                Console.WriteLine("Telefono: " & reserva1(1))
+                Console.WriteLine("Cantidad de personas: " & reserva1(2))
+                Console.WriteLine("Numero de comensales: " & reserva1(3))
+                Console.WriteLine("Rut: " & reserva1(4))
+                ' Falta asignar la mesa al usuario depeniendo la cantidad 
+                ' de comensales ingresados
+                Console.WriteLine("Numero de mesa: ")
+        End Select
+
+
 
     End Sub
 
